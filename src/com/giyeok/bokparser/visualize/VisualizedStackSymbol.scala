51be58f680a5289c8b2de478bb2b5f934a294714
Joonsoo Jeon <giyeok7@gmail.com>
2013-02-15 00:19:43 +0900

    VisualizedStackSymbol design improved
    TODO: dangled whitespace.
    - for test case " (1) + 2;", it is parsed in two ways because of the
    whitespaces

--- src/com/giyeok/bokparser/visualize/VisualizedStackSymbol.scala
+++ src/com/giyeok/bokparser/visualize/VisualizedStackSymbol.scala
@@ -2,30 +2,32 @@ package com.giyeok.bokparser.visualize
 
 import org.eclipse.draw2d.ColorConstants
 import org.eclipse.draw2d.Figure
+import org.eclipse.draw2d.FigureCanvas
 import org.eclipse.draw2d.Label
 import org.eclipse.draw2d.LineBorder
+import org.eclipse.draw2d.MouseEvent
+import org.eclipse.draw2d.MouseMotionListener
 import org.eclipse.draw2d.ToolbarLayout
+import org.eclipse.swt.graphics.Color
+import org.eclipse.swt.layout.FillLayout
+import org.eclipse.swt.widgets.Display
+import org.eclipse.swt.widgets.Shell
+
+import com.giyeok.bokparser.BlackboxParser
 import com.giyeok.bokparser.CharInputSymbol
 import com.giyeok.bokparser.EOFSymbol
 import com.giyeok.bokparser.EmptySymbol
+import com.giyeok.bokparser.Grammar
 import com.giyeok.bokparser.NontermSymbol
 import com.giyeok.bokparser.Nonterminal
+import com.giyeok.bokparser.ParseSuccess
+import com.giyeok.bokparser.Parser
+import com.giyeok.bokparser.ParserInput
 import com.giyeok.bokparser.StackSymbol
 import com.giyeok.bokparser.StartSymbol
 import com.giyeok.bokparser.TermSymbol
 import com.giyeok.bokparser.VirtInputSymbol
 import com.giyeok.bokparser.grammars.JavaScriptGrammar
-import com.giyeok.bokparser.ParserInput
-import org.eclipse.swt.widgets.Shell
-import org.eclipse.swt.widgets.Display
-import com.giyeok.bokparser.Parser
-import com.giyeok.bokparser.BlackboxParser
-import org.eclipse.draw2d.FigureCanvas
-import com.giyeok.bokparser.ParseSuccess
-import org.eclipse.swt.layout.FillLayout
-import org.eclipse.draw2d.MouseMotionListener
-import org.eclipse.draw2d.MouseEvent
-import com.giyeok.bokparser.Grammar
 
 object VisualizedStackSymbol {
 	def main(args: Array[String]) {
@@ -60,83 +62,111 @@ object VisualizedStackSymbol {
 }
 
 // Show tree structure of given StackSymbol visually
-class VisualizedStackSymbol(val stackSymbol: StackSymbol, val whitespace: Boolean = false, val grammar: Grammar = null) extends Figure {
-	private val _layout = new ToolbarLayout(false)
+class VisualizedStackSymbol(val stackSymbol: StackSymbol, val whitespace: Boolean = false, val grammar: Grammar = null, borderColor: Color = ColorConstants.darkGray) extends Figure {
+	{
+		val _layout = new ToolbarLayout(false)
 
-	stackSymbol match {
-		case StartSymbol => add(new Label("$"))
-		case NontermSymbol(item) =>
-			def layoutManager(spacing: Int = 2) = {
-				val layout = new ToolbarLayout(true)
-				layout.setSpacing(spacing)
-				layout
-			}
+		val whitespaceBoxColor = ColorConstants.blue
+		val normalBoxColor = ColorConstants.darkGray
 
-			item.item match {
-				case Nonterminal(name, _, _) =>
-					val nonterm = new Label(name)
-					if (grammar != null) {
-						nonterm.setToolTip(new RuleFigure((name, grammar.rules(name))))
+		stackSymbol match {
+			case StartSymbol => add(new Label("$"))
+			case NontermSymbol(item) =>
+				item.item match {
+					case Nonterminal(name, _, _) =>
+						val nonterm = new Label(name)
+						if (grammar != null) {
+							nonterm.setToolTip(new RuleFigure((name, grammar.rules(name))))
+						}
+						add(nonterm)
+					case _ =>
+				}
+
+				val fig = new Figure
+				fig.setLayoutManager({
+					val layout = new ToolbarLayout(true)
+					layout.setSpacing(2)
+					layout
+				})
+				fig.setBorder(null)
+
+				if (whitespace) {
+					item.precedingWS.foreach((x) => fig.add(new VisualizedStackSymbol(x, whitespace, grammar, whitespaceBoxColor)))
+					if (!(item.precedingWS isEmpty)) {
+						fig.add(new Label("|"))
 					}
-					add(nonterm)
-				case _ =>
-			}
+				}
 
-			val children = new Figure
-			children.setLayoutManager(layoutManager())
+				if (whitespace && (item.isInstanceOf[Parser#StackEntry#ParsingSequence])) {
+					val s = item.asInstanceOf[Parser#StackEntry#ParsingSequence]
+					def x(children: List[StackSymbol], indices: List[Int], pointer: Int): Unit =
+						indices match {
+							case i :: is =>
+								if (pointer == i) {
+									fig.add(new VisualizedStackSymbol(children.head, whitespace, grammar, normalBoxColor))
+									x(children.tail, is, pointer + 1)
+								} else {
+									fig.add(new VisualizedStackSymbol(children.head, whitespace, grammar, whitespaceBoxColor))
+									x(children.tail, indices, pointer + 1)
+								}
+							case _ =>
+								if (!(children isEmpty)) {
+									fig.add(new VisualizedStackSymbol(children.head, whitespace, grammar, whitespaceBoxColor))
+									x(children.tail, indices, pointer + 1)
+								}
+						}
+					x(s.childrenWithWS, s.indexNonWS, 0)
+				} else {
+					item.children.foreach((x) => fig.add(new VisualizedStackSymbol(x, whitespace, grammar, normalBoxColor)))
+				}
 
-			if (whitespace) {
-				val precedingWS = new Figure
-				precedingWS.setLayoutManager(layoutManager())
-				precedingWS.setBorder(new LineBorder(ColorConstants.gray, 1))
-				item.precedingWS.foreach((x) => precedingWS.add(new VisualizedStackSymbol(x, whitespace, grammar)))
-				children add precedingWS
-			}
+				if (whitespace) {
+					if (!(item.followingWS isEmpty)) {
+						fig.add(new Label("|"))
+					}
+					item.followingWS.foreach((x) => fig.add(new VisualizedStackSymbol(x, whitespace, grammar, whitespaceBoxColor)))
+				}
 
-			val content = new Figure
-			content.setLayoutManager(layoutManager())
-			content.setBorder(new LineBorder(ColorConstants.darkGray, 1))
-			item.children.foreach((x) => content.add(new VisualizedStackSymbol(x, whitespace, grammar)))
-			children add content
+				_layout.setSpacing(2)
+				add(fig)
+			case TermSymbol(input, pointer) =>
+				input match {
+					case CharInputSymbol(char) => add(new Label(s"'$char'"))
+					case VirtInputSymbol(name) => add(new Label(name))
+					case EOFSymbol => add(new Label("$"))
+				}
+			case EmptySymbol => add(new Label("()"))
+		}
 
-			if (whitespace) {
-				val followingWS = new Figure
-				followingWS.setLayoutManager(layoutManager())
-				followingWS.setBorder(new LineBorder(ColorConstants.gray, 1))
-				item.followingWS.foreach((x) => followingWS.add(new VisualizedStackSymbol(x, whitespace, grammar)))
-				children add followingWS
-			}
+		setLayoutManager(_layout)
 
-			_layout.setSpacing(2)
-			add(children)
-		case TermSymbol(input, pointer) =>
-			input match {
-				case CharInputSymbol(char) => add(new Label(s"'$char'"))
-				case VirtInputSymbol(name) => add(new Label(name))
-				case EOFSymbol => add(new Label("$"))
-			}
-		case EmptySymbol => add(new Label("()"))
+		// set border for nonterminals
+		stackSymbol match {
+			case NontermSymbol(item) =>
+				item.item match {
+					case Nonterminal(_, _, _) =>
+						val highlightedBorder = new LineBorder(ColorConstants.red, 1)
+						val normalBorder = new LineBorder(borderColor, 1)
+						setBorder(normalBorder)
+
+						addMouseMotionListener(new MouseMotionListener {
+							def mouseDragged(e: MouseEvent) {}
+
+							def mouseEntered(e: MouseEvent) {
+								setBorder(highlightedBorder)
+							}
+
+							def mouseExited(e: MouseEvent) {
+								setBorder(normalBorder)
+							}
+
+							def mouseHover(e: MouseEvent) {}
+
+							def mouseMoved(e: MouseEvent) {}
+						})
+					case _ =>
+				}
+			case _ =>
+		}
 	}
-
-	setLayoutManager(_layout)
-
-	private val highlightedBorder = new LineBorder(ColorConstants.red, 1)
-	private val normalBorder = new LineBorder(ColorConstants.black, 1)
-	setBorder(normalBorder)
-
-	addMouseMotionListener(new MouseMotionListener {
-		def mouseDragged(e: MouseEvent) {}
-
-		def mouseEntered(e: MouseEvent) {
-			setBorder(highlightedBorder)
-		}
-
-		def mouseExited(e: MouseEvent) {
-			setBorder(normalBorder)
-		}
-
-		def mouseHover(e: MouseEvent) {}
-
-		def mouseMoved(e: MouseEvent) {}
-	})
 }
