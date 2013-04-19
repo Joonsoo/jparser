85f809117c62c1048f51bebd65b81c5aca8260f2
Joonsoo Jeon <giyeok7@gmail.com>
2013-04-20 01:47:57 +0900

    Grammar action process routine implemented

--- src/com/giyeok/bokparser/StackSymbol.scala
+++ src/com/giyeok/bokparser/StackSymbol.scala
@@ -5,10 +5,14 @@ import com.giyeok.bokparser.dynamic.Parser
 sealed abstract class StackSymbol {
 	val text: String
 	val source: List[InputSymbol]
+	
+	val processed: Object
 }
 case object StartSymbol extends StackSymbol {
 	val text = ""
 	val source = Nil
+	
+	val processed = this
 }
 case class NontermSymbol(item: Parser#StackEntry#ParsingItem) extends StackSymbol {
 	lazy val text = (item.children map (_ text)) mkString
@@ -19,12 +23,25 @@ case class NontermSymbol(item: Parser#StackEntry#ParsingItem) extends StackSymbo
 		}
 		rec(item.children)
 	}
+	
+	lazy val processed = {
+		val objs = item.children map (_.processed)
+		item.item.action(this, objs)
+	}
 }
 case class TermSymbol(input: InputSymbol, pointer: Int) extends StackSymbol {
-	val text: String = input match { case CharInputSymbol(c) => String valueOf c case _ => "" }
+	val text: String = input match { 
+		case CharInputSymbol(c) => String valueOf c
+		case TokenInputSymbol(token) => token text
+		case _ => "" 
+	}
 	val source = List(input)
+	
+	val processed = this
 }
 case object EmptySymbol extends StackSymbol {
 	val text = ""
 	val source = Nil
+
+	val processed = this
 }
