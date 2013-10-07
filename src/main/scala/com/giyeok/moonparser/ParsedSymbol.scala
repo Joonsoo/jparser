5b607c1f0f80ce86b8374bcc2ab5f5d1ee425fbb
Joonsoo Jeon <giyeok7@gmail.com>
2013-10-07 21:13:56 +0900

    Removed unnecessary equals and canEqual methods from ParsedSymbols   - Scala automatically inserts hashCode and equals methods for case classes   - but the default hashCode seems to have some problem

--- src/main/scala/com/giyeok/moonparser/ParsedSymbol.scala
+++ src/main/scala/com/giyeok/moonparser/ParsedSymbol.scala
@@ -19,11 +19,6 @@ object ParsedSymbols {
         val source = Nil
 
         override lazy val hashCode = elem.hashCode
-        override def equals(other: Any) = other match {
-            case that: EmptySymbol => (that canEqual this) && (elem == that.elem)
-            case _ => false
-        }
-        def canEqual(other: Any) = other.isInstanceOf[EmptySymbol]
     }
     case class TermSymbol[+T <: Input](input: T, pointer: Int) extends ParsedSymbol with ConcreteSymbol {
         lazy val text = input match {
@@ -35,11 +30,6 @@ object ParsedSymbols {
         lazy val source = Seq(input)
 
         override lazy val hashCode = (input, pointer).hashCode
-        override def equals(other: Any) = other match {
-            case that: TermSymbol[T] => (that canEqual this) && (input == that.input) && (pointer == that.pointer)
-            case _ => false
-        }
-        def canEqual(other: Any) = other.isInstanceOf[TermSymbol[T]]
     }
     abstract class NontermSymbol extends ParsedSymbol with NamedSymbol {
         val elem: GrElem
@@ -50,12 +40,6 @@ object ParsedSymbols {
         lazy val source = child match { case c: ConcreteSymbol => c.source case _ => Nil }
 
         override lazy val hashCode = (elem, child).hashCode
-        override def equals(other: Any) = other match {
-            case that: NontermSymbolElem =>
-                (that canEqual this) && (elem == that.elem) && (child == that.child)
-            case _ => false
-        }
-        def canEqual(other: Any) = other.isInstanceOf[NontermSymbolElem]
     }
     case class NontermSymbolSeq(elem: GrElem, children: Seq[ParsedSymbol])
             extends NontermSymbol with ConcreteSymbol {
@@ -63,12 +47,6 @@ object ParsedSymbols {
         lazy val source = children flatMap { case c: ConcreteSymbol => c.source case _ => Nil }
 
         override lazy val hashCode = (elem, children).hashCode
-        override def equals(other: Any) = other match {
-            case that: NontermSymbolSeq =>
-                (that canEqual this) && (elem == that.elem) && (children == that.children)
-            case _ => false
-        }
-        def canEqual(other: Any) = other.isInstanceOf[NontermSymbolSeq]
     }
     class NontermSymbolSeqWS(elem: GrElem, children: Seq[ParsedSymbol], val childrenWS: Seq[ParsedSymbol], val mappings: Map[Int, Int])
             extends NontermSymbolSeq(elem, children) {
@@ -76,13 +54,6 @@ object ParsedSymbols {
         override lazy val source = childrenWS flatMap { case c: ConcreteSymbol => c.source case _ => Nil }
 
         override lazy val hashCode = (elem, children, childrenWS, mappings).hashCode
-        override def equals(other: Any) = other match {
-            case that: NontermSymbolSeqWS =>
-                (that canEqual this) && (elem == that.elem) && (children == that.children) &&
-                    (childrenWS == that.childrenWS) && (mappings == that.mappings)
-            case _ => false
-        }
-        override def canEqual(other: Any) = other.isInstanceOf[NontermSymbolSeqWS]
     }
 
     // TODO implement virtual symbols for static analysis of the grammar
@@ -102,10 +73,5 @@ object ParsedSymbols {
             }
 
         override lazy val hashCode = (source, compats).hashCode
-        override def equals(other: Any) = other match {
-            case that: Token => (that canEqual this) && (source == that.source) && (compats == that.compats)
-            case _ => false
-        }
-        override def canEqual(other: Any) = other.isInstanceOf[Token]
     }
 }
