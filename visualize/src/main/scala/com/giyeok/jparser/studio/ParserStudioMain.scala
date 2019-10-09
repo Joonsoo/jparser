ee9b34808e238927271eb75779a8d9cb2f75d16e
Joonsoo Jeon <giyeok7@gmail.com>
2019-10-09 13:39:06 +0900

    Working on Java and TFDL grammar

--- visualize/src/main/scala/com/giyeok/jparser/studio/ParserStudioMain.scala
+++ visualize/src/main/scala/com/giyeok/jparser/studio/ParserStudioMain.scala
@@ -2,8 +2,6 @@ package com.giyeok.jparser.studio
 
 import com.giyeok.jparser.examples.metalang2.ExpressionGrammarsMetaLang2
 import com.giyeok.jparser.examples.{AllExamples, AmbiguousExamples, GrammarWithExamples, StringExamples}
-import com.giyeok.jparser.metalang.MetaGrammar
-import com.giyeok.jparser.metalang2.MetaLanguage2
 
 object ParserStudioMain {
     def main(args: Array[String]): Unit = {
@@ -22,7 +20,7 @@ object ParserStudioMain {
         }
 
         val allExamples = AllExamples.grammarWithExamples // ++
-            // AllExamples.metaLangsToGrammar(MetaGrammar.translateForce, MetaLanguage2.grammarSpecToGrammar(_, _).get)
+        // AllExamples.metaLangsToGrammar(MetaGrammar.translateForce, MetaLanguage2.grammarSpecToGrammar(_, _).get)
 
         val initial = ExpressionGrammarsMetaLang2.basic
         ParserStudio.start(initial.grammar, initial.correctExamples.head, allExamples map { g => grammar(g.grammar.name, g) })
