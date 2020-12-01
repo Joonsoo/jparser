ecde41458867a65a4e8bfca776b0f0f2bab52330
Joonsoo Jeon <giyeok7@gmail.com>
2020-12-01 23:13:29 +0900

    old metalang3 removed

--- metalang/src/main/scala/com/giyeok/jparser/metalang3/codegen/ProtobufTypesGen.scala
+++
@@ -1,6 +0,0 @@
-package com.giyeok.jparser.metalang3.codegen
-
-// Protobuf는 파싱 결과를 저장하는 포맷 정의 파일만 생성. AST의 Valueify는 다른 언어를 이용
-class ProtobufTypesGen {
-
-}
--- metalang/src/main/scala/com/giyeok/jparser/metalang3/generated/Test.scala
+++
@@ -1,7 +0,0 @@
-package com.giyeok.jparser.metalang3.generated
-
-import com.giyeok.jparser.ParseResultTree.{BindNode, Node, SequenceNode}
-import com.giyeok.jparser.metalang2.generated.MetaGrammar3Ast.TypeDesc
-
-class Test {
-}
