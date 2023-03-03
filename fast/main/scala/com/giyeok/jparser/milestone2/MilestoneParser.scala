57b6f82bed5b35223441c329c33b0026a91e9bd6
Joonsoo Jeon <giyeok7@gmail.com>
2023-03-03 10:56:17 +0900

    parseOrThrow method added to MilestoneParser

--- fast/main/scala/com/giyeok/jparser/milestone2/MilestoneParser.scala
+++ fast/main/scala/com/giyeok/jparser/milestone2/MilestoneParser.scala
@@ -283,6 +283,8 @@ class MilestoneParser(val parserData: MilestoneParserData) {
     }
   }
 
+  def parseOrThrow(source: String): ParsingContext = parseOrThrow(Inputs.fromString(source))
+
   // progress되면서 추가된 커널들을 반환한다. finish는 progress 되면서 자연스럽게 따라오는 것이기 때문에 처리할 필요 없음
   def kernelsHistory(parsingContext: ParsingContext): List[Set[Kernel]] = {
     def progressAndMapGen(kernel: Kernel, gen: Int, genMap: Map[Int, Int]): Kernel =
