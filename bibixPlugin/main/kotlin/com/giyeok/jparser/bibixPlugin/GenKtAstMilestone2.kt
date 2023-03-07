16a08054c70ae5a1c0141cb4e471fb5eee9e8eba
Joonsoo Jeon <giyeok7@gmail.com>
2023-03-07 14:16:23 +0900

    unnecessary code removed

--- bibixPlugin/main/kotlin/com/giyeok/jparser/bibixPlugin/GenKtAstMilestone2.kt
+++ bibixPlugin/main/kotlin/com/giyeok/jparser/bibixPlugin/GenKtAstMilestone2.kt
@@ -11,49 +11,6 @@ import java.nio.file.Path
 import kotlin.io.path.*
 
 class GenKtAstMilestone2 {
-  companion object {
-    @JvmStatic
-    fun main(args: Array<String>) {
-      val ctx = BuildContext(
-        BuildEnv(OS.MacOSX("", ""), Architecture.Aarch_64),
-        FileSystems.getDefault(),
-        Path("."),
-        null,
-        null,
-//      mapOf(
-//        "cdgFile" to FileValue(Path("../j1/grammar/grammar.cdg")),
-//        "astifierClassName" to StringValue("J1Ast"),
-//        "parserDataFileName" to StringValue("j1-parserdata.pb"),
-//        "trimParserData" to BooleanValue(true),
-//      ),
-        mapOf(
-          "cdgFile" to FileValue(Path("examples/main/resources/cdglang3.cdg")),
-          "astifierClassName" to StringValue("MetaLang3Ast"),
-          "parserDataFileName" to StringValue("cdglang3-parserdata.pb"),
-          "trimParserData" to BooleanValue(false),
-        ),
-        true,
-        targetIdData { },
-        "",
-        Path("bbxbuild/objects/abc"),
-        object : ProgressLogger {
-          override fun logError(message: String) {
-          }
-
-          override fun logInfo(message: String) {
-            println(message)
-          }
-        },
-        object : BaseRepo {
-          override fun prepareSharedDirectory(sharedRepoName: String): Path {
-            TODO("Not yet implemented")
-          }
-        }
-      )
-      println(GenKtAstMilestone2().build(ctx))
-    }
-  }
-
   fun build(context: BuildContext): BibixValue {
     context.progressLogger.logInfo("GenKtAstMilestone2 started (dest=${context.destDirectory})")
     val cdgDef = (context.arguments.getValue("cdgFile") as FileValue).file.readText()
